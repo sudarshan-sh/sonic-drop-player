6c9d6ab06d0da17f7cc9260072a447e99efd36b0
sudarshan-sh <mail2sudarshan.sharma@gmail.com>
2026-09-15 21:33:36 +0530

    port updated to 8000

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/api/playlists", playlistsRoutes);
 
 app.use(cors()); // to allow cross-origin requests
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on the port: ${PORT}`);
