2e7284671945b6b48b0472b8f1046bf0c4023686
sudarshan-sh <mail2sudarshan.sharma@gmail.com>
2026-09-16 12:39:07 +0530

    added navigation on the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -27,7 +27,12 @@ const Navbar = ({
     <div>
       <nav className="bg-gray-800 p-4">
         <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-white font-bold text-xl">Sonic Drop Player</h1>
+          <h1
+            className="text-white font-bold text-xl cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            Sonic Drop Player
+          </h1>
           <div>
             {user ? (
               <>
